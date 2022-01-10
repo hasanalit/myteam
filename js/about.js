dfb37b5694b116e8781b18258d253394bfcd7334
hasanalit <tuxtayevhasan72@gmail.com>
2022-01-10 16:22:16 +0500

    about-meet-first-animation

---
+++ js/about.js
@@ -0,0 +1,41 @@
+// let elList = document.querySelector('.about-meet__list')
+// let elAboutBtn = document.querySelector('.about-meet__btn')
+// let elItem = document.querySelector('.about-meet__item')
+// let elAboutMeetImg = document.querySelector('about-meet__img')
+// let elAboutMeetName = document.querySelector('about-meet__name')
+// let elAboutMeetDesc = document.querySelector('about-meet__desc')
+
+
+
+// elAboutBtn.addEventListener('click', ()=>{
+
+
+
+
+
+
+//     // let newName = document.createElement('p')
+//     // let newDesc = document.createElement('p')
+//     // let newSection = document.createElement('div')
+//     // let newFirstImg = document.createElement('img')
+//     // let newSecondImg = document.createElement('img')
+
+
+//     // // SET-ATRIBUTE
+//     // newName.setAttribute('class', 'about-name')
+//     // newSection.setAttribute('class', 'about-desc')
+//     // newDesc.setAttribute('class', 'about-section')
+//     // // newFirstImg.setAttribute('class', 'about-first-img')
+//     // // newSecondImg.setAttribute('class', 'about-second-img')
+//     // // newFirstImg.setAttribute('src', images/twitter.svg)
+
+
+//     // // CONTENT
+//     // newName.textContent = 'Aden Allan'
+//     // newDesc.textContent = '“Empowered teams create truly amazing products. Set the north star and let them follow it.”'
+
+
+//     // //APPEND
+//     // elItem.appendChild(newName)
+//     // elItem.appendChild(newDesc)
+// })
